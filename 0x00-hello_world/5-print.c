3480f9633d3e7b6661d30477ec8a55efa3b1b9e2
Possible-1 <josephakintola012@gmail.com>
2022-12-17 01:48:51 -0800

    alx low level language

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,11 @@
+#inclcude <stdio.h>
+/**
+ * main - this should be void
+ * descripton - this should print using printf function
+ * return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar\, but the outcome is a piece of art\,")
+	return(0)
+}
