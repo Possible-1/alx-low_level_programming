580bf27b62d4c9812c6c645244795a697dccb3aa
Possible-1 <josephakintola012@gmail.com>
2022-12-17 01:40:02 -0800

    alx low level language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * descritption: This prints using puts function
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
