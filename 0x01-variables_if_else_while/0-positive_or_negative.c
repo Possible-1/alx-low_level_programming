bf87910672a644e84ff4db4cb1875286241bad72
Possible-1 <josephakintola012@gmail.com>
2022-12-28 03:10:45 -0800

    If, else and while function projects

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,18 +2,21 @@
 #include <time.h>
 #include <stdio.h>
 
-/** main - This code begins at main
+/**
+ * main - Entry point
  * Description : This code prints random numbers
- * while numbers greater than 0 is positive
- * 0 is zero
+ * while numbers greater than zero is positive
+ * zero is zero
  * number less than 0 is negative
- * Return : Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int n;
-	srand(time(0));
+
+	srand(time(0)\n);
 	n = rand() - RAND_MAX / 2;
+	/* your code goes here*/
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
