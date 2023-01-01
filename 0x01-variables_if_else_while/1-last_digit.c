0bd10e8393027716e19dc54a794b9d323a29fd09
Possible-1 <josephakintola012@gmail.com>
2023-01-01 07:49:20 -0800

    If, else and while function projects

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 		}
 		else if (n < 6)
 		{
-			printf("Last digit of %d is %d and is less 6 and not 0\n", n, l);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 		}
 		return (0);
 	}
